7d4f8a767a6f292d9c881ac26ee83e04cd943edd
gabewolford <gabewolford@gmail.com>
2023-07-22 13:20:45 -0700

    swapped new color classes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main className="m-6 flex flex-col">
       <header className="w-full">
-          <h1 className="mb-3 tracking-tight text-blue-300 text-5xl md:text-7xl lg:text-8xl">Biiig&nbsp;<span className="tracking-widest">Stretch</span></h1>
+          <h1 className="mb-3 tracking-tight text-rust text-5xl md:text-7xl lg:text-8xl">Biiig&nbsp;<span className="tracking-widest text-light-blue">Stretch</span></h1>
       </header>
       <div className="grid grid-cols-1 mb-10 md:col-span-4 lg:col-span-2">
           <h2 className="text-3xl md:text-4xl lg:text-5xl">
@@ -17,7 +17,7 @@ export default function HomePage() {
       </div>
       
       <section className="mb-10">
-        <h2 className="text-xs tracking-widest mb-4 text-blue-300">HIGHLIGHTED WORK</h2>
+        <h2 className="text-xs tracking-widest mb-4 text-gold">HIGHLIGHTED WORK</h2>
         <ul>
           <Link href="#" target="_blank" className="chunderline">
             <li>Coming soon...</li>
@@ -27,12 +27,13 @@ export default function HomePage() {
 
       <section className="flex flex-col md:flex-row w-full mb-10">
         <div className="md:mr-16 mb-10 md:mb-0">
-          <h2 className="text-xs tracking-widest mb-4 text-blue-300">SERVICES</h2>
+          <h2 className="text-xs tracking-widest mb-4 text-gold">SERVICES</h2>
             <ul>
               <li>Full Stack Development</li>
               <li>Front-End Development</li>
               <li>Headless Development</li>
               <li>Marketing & Portfolio Sites</li>
+              <li>Landing Pages</li>
               <li>Domains & Hosting</li>
               <li>Webmaster Services</li>
               <li>Technical SEO</li>
@@ -42,7 +43,7 @@ export default function HomePage() {
             </ul>
         </div>
         <div>
-          <h2 className="text-xs tracking-widest mb-4 text-blue-300">TECHNOLOGIES</h2>
+          <h2 className="text-xs tracking-widest mb-4 text-gold">TECHNOLOGIES</h2>
           <ul>
             <li>NextJS</li>
             <li>React</li>
@@ -53,6 +54,7 @@ export default function HomePage() {
             <li>HTML</li>
             <li>CSS</li>
             <li>Javascript</li>
+            <li>Typescript</li>
             <li>& more</li>
           </ul>
         </div>
@@ -60,7 +62,7 @@ export default function HomePage() {
 
       <section className="flex flex-col md:flex-row w-full">
         <div className="md:mr-16 mb-10 md:mb-0">
-          <h2 className="text-xs tracking-widest mb-2 text-blue-300">HAVE A JOB FOR US?</h2>
+          <h2 className="text-xs tracking-widest mb-2 text-gold">HAVE A JOB FOR US?</h2>
           <Link href="mailto:gabe@biiigstretch.studio">
             <h2 className="chunderline">Get in touch.</h2>
           </Link>
