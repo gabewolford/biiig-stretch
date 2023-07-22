653386248cc6c0df2d7d23aa75e942cee432f8a3
gabewolford <gabewolford@gmail.com>
2023-07-22 13:20:31 -0700

    imported google font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,19 @@
 import './globals.css'
+import { Hanken_Grotesk } from 'next/font/google'
 
 export const metadata = {
   title: 'Biiig Stretch Studio | Web Development',
-  description: 'is a purposefully small design-oriented development studio based somewhere in the Pacific Wonderland.',
+  description: 'A purposefully small design-oriented development studio based in the heart of the Pacific Wonderland.',
 }
+
+const hanken = Hanken_Grotesk({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={hanken.className}>
       <body>
         {children}
       </body>
