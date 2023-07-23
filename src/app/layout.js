d653f5cca8db4dcd527b8ed583f5cba67b7939d0
gabewolford <gabewolford@gmail.com>
2023-07-22 17:45:49 -0700

    font fix?!

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,11 @@
 import './globals.css'
 import { Hanken_Grotesk } from 'next/font/google'
-import localFont from 'next/font/local'
+
 
 export const metadata = {
   title: 'Biiig Stretch Studio | Web Development',
   description: 'A purposefully small design-oriented development studio based in the heart of the Pacific Wonderland.',
 }
-
-export const knockout = localFont({
-  src: './fonts/Liteweight.ttf',
-  display: 'swap'
-})
 
 export const hanken = Hanken_Grotesk({
   subsets: ['latin'],
