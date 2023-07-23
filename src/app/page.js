4b3f0c4ad24efb0acb074f1cbd1620a1b4e15cef
gabewolford <gabewolford@gmail.com>
2023-07-22 18:41:38 -0700

    grid/flex box fixed

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <main className="grid mx-8 my-14 md:mx-16 lg:my-40 gap-x-8 grid-cols-2 md:grid-cols-4 lg:grid-cols-12">
       
-      <header className="col-span-2 md:col-span-4 lg:col-start-2 lg:col-span-12 mb-16 lg:mb-32">
+      <header className="col-span-2 md:col-span-6 lg:col-start-2 lg:col-span-12 mb-16 lg:mb-32">
           <h1 className="hidden">Biiig Stretch</h1>
           <figure>
             <Image 
@@ -30,24 +30,25 @@ export default function HomePage() {
           </section>
       </header>
     
-      <div className="grid col-span-2 lg:col-start-2 md:col-span-6 md:gap-8 mb-8 md:mb-12">
-        <section className="col-start-1 flex flex-col mb-8">
+
+      <div className="col-start 1 col-span-2 md:col-span-6 lg:col-start-2 lg:col-span-12 flex flex-col lg:flex-row">
+        <section className="mb-8 lg:mr-20">
           <header className="text-gold mb-2 md:mb-4 liteweight text-2xl md:text-3xl">Services</header>
             <ul>
-              <li>Full Stack Development</li>
-              <li>Front-End Development</li>
-              <li>Headless Development</li>
-              <li>Marketing & Portfolio Sites</li>
-              <li>Landing Pages</li>
-              <li>Domains & Hosting</li>
-              <li>Webmaster Services</li>
-              <li>Technical SEO</li>
-              <li>Consulting & Support</li>
-              <li>Digital Strategy</li>
+              <li>Full&nbsp;Stack&nbsp;Development</li>
+              <li>Front-End&nbsp;Development</li>
+              <li>Headless&nbsp;Development</li>
+              <li>Marketing&nbsp;&&nbsp;Portfolio&nbsp;Sites</li>
+              <li>Landing&nbsp;Pages</li>
+              <li>Domains&nbsp;&&nbsp;Hosting</li>
+              <li>Webmaster&nbsp;Services</li>
+              <li>Technical&nbsp;SEO</li>
+              <li>Consulting&nbsp;&&nbsp;Support</li>
+              <li>Digital&nbsp;Strategy</li>
               <li>& more</li>
             </ul>
         </section>
-        <section className="col-start-1 md:col-start-2 lg:col-start-4 flex flex-col">
+        <section className="mb-8 lg:mr-36">
           <header className="text-gold font-medium mb-2 md:mb-4 liteweight text-2xl md:text-3xl">Technologies</header>
             <ul>
               <li>NextJS</li>
@@ -63,25 +64,26 @@ export default function HomePage() {
               <li>& more</li>
             </ul>
         </section>
-      </div>
 
-      <section className="grid col-span-2 md:col-span-4 lg:col-start-2 lg:col-span-12 gap-4">
-        <header className="text-gold font-medium liteweight text-2xl md:text-3xl">Highlighted work</header>
-        <article className="flex flex-col gap-1 w-full">
-          <Link
-            href="https://islamoradafishingguidesandcharters.com"
-            className="text-[18px] font-semibold"
-            target="_blank"
-          >
-            Islamorada Fishing Guides
-          </Link>
-          <p className="text-sm">Full Stack Development, Webmaster Services, Domains & Hosting, NextJS, React, Sanity, Vercel</p>
-          <p className="text-sm">2023</p>
-        </article>
-        <article className="flex flex-col gap-1 w-full">
-          <h3 className="text-[18px] font-semibold">More coming soon...</h3>
-        </article>
-      </section>
+        <section className="gap-4">
+          <header className="text-gold font-medium liteweight text-2xl md:text-3xl mb-2 md:mb-4">Highlighted work</header>
+          <article className="flex flex-col gap-1 mb-2 w-full">
+            <Link
+              href="https://islamoradafishingguidesandcharters.com"
+              className="text-[18px] font-semibold"
+              target="_blank"
+            >
+              Islamorada Fishing Guides
+            </Link>
+            <p className="text-sm">Full Stack Development, Webmaster Services, Domains & Hosting, NextJS, React, Sanity, Vercel</p>
+            <p className="text-sm">2023</p>
+          </article>
+          <article className="flex flex-col gap-1 w-full">
+            <h3 className="text-[18px] font-semibold">More coming soon...</h3>
+          </article>
+        </section>
+      </div>
+      
   
     </main>
   )
