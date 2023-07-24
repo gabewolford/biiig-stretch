d76960a935677b57e3e8661a0361f9314d6cdb8f
gabewolford <gabewolford@gmail.com>
2023-07-24 10:59:26 -0700

    installed analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Hanken_Grotesk } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 
 export const metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={hanken.className}>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   )
